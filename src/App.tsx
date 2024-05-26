fc9d7fab5e5e39526ca8f434fa0316294204bc9a
helga-umrikh <zetaumr@gmail.com>
2024-05-26 23:46:33 +0300

    add task list and task form to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './App.scss'
+import { Layout } from "antd";
+import { Content, Footer, Header } from 'antd/es/layout/layout';
+import TodoList from './components/TodoList/TodoList';
+import TodoForm from './components/TodoForm/TodoForm';
+import Title from 'antd/es/typography/Title';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Layout className="App">
+      <Header className='header'>
+        <Title className='header__title' level={2}>TO-DO LIST</Title>
+      </Header>
+      <Content className='content'>
+        <TodoForm />
+        <TodoList/>
+      </Content>
+      <Footer />
+    </Layout>
   );
 }
 
